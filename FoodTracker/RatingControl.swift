e2b64a4243a9b37d04922cfcd92c00ccadffa3fd
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-10-18 14:06:36 +0200

    Add basic UI

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -17,5 +17,47 @@ class RatingControl: UIView {
         // Drawing code
     }
     */
+    
+    // MARK: Properties
+    
+    let spacing = 5
+    let starCount = 5
+    let size = 44
+    
+    var rating = 0;
+    var ratingButtons = [UIButton]()
+    
+    // MARK: Initialization
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        for _ in 0..<starCount {
+            let button = UIButton(frame: CGRect(x: 0, y: 0, width: size, height: size))
+            button.backgroundColor = UIColor.red
+            button.addTarget(self, action: #selector(ratingButtonTapped(button:)), for: .touchDown)
+            ratingButtons += [button]
+            addSubview(button)
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 240, height: 44)
+    }
+    
+    override func layoutSubviews() {
+        var buttonFrame = CGRect(x: 0, y: 0, width: size, height: size)
+        
+        for(index, button) in ratingButtons.enumerated() {
+            buttonFrame.origin.x = CGFloat(index * (size + spacing))
+            button.frame = buttonFrame
+        }
+    }
+    
+    // MARK: Button Action
+    
+    func ratingButtonTapped(button: UIButton) {
+        print("Button \(ratingButtons.index(of: button)!) pressed 👍")
+    }
 
 }
--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,18 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var mealName: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    // MARK: Overriden functions
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mealName.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: Actions
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        mealName.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+        mealName.text = ""
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        photoImageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
 
 }
 
