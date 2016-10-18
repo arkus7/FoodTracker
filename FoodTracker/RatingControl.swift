22d840272528dbcd97df3cd08e157002b135081b
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-10-18 14:04:05 +0200

    Init commit

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Arkadiusz Żmudzin on 13.10.2016.
+//  Copyright © 2016 Arkadiusz Żmudzin. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
